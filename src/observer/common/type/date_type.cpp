0ac05888fdbe3092d7aeeffacad8c265d114a135
ZNOASIS ZHANG <1978236639@qq.com>
2024-11-20 15:13:42 +0000

    1

---
+++ src/observer/common/type/date_type.cpp
@@ -0,0 +1,9 @@
+#include <iomanip>
+
+#include "common/lang/comparator.h"
+#include "common/lang/sstream.h"
+#include "common/log/log.h"
+#include "common/type/date_type.h"
+#include "common/value.h"
+
+
---
+++ src/observer/common/type/date_type.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "common/type/data_type.h"
+
+class DateType
+{
+public:
+
+    
+};
