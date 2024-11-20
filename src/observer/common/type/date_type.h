c3d773746e28b360027e479b45d88431a58d6561
ZNOASIS ZHANG <1978236639@qq.com>
2024-11-20 15:47:04 +0000

    2

--- src/observer/common/type/date_type.h
+++ src/observer/common/type/date_type.h
@@ -5,6 +5,22 @@
 class DateType
 {
 public:
+  Date() = default;
+  Date(const char *str);
+  Date(int data);
+  // Date(int year,int month,int day);
 
-    
+  bool        is_valid() const { return valid; }
+  bool        operator<(const Date &that) const;
+  bool        operator==(const Date &that) const;
+  int         compare(const Date &that) const;
+  std::string to_string() const;
+  int         to_int() const;
+  void        parse_date_int(int date);
+  bool        is_date_valid() const;
+  bool        valid{};
+  int         year{};
+  int         month{};
+  int         day{};
+  std::string format(const std::string &format_str) const；
 };
